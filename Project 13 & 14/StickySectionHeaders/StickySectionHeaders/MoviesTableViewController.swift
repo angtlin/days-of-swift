130809c0a014fd73dea1a836876be3d6375b3a4f
Angela Lin <angelalin@L22-7.tpml.edu.tw>
2017-01-24 11:25:52 +0800

    Fix sorted data to go under tableview sections, add section index

--- Project 13 & 14/StickySectionHeaders/StickySectionHeaders/MoviesTableViewController.swift
+++ Project 13 & 14/StickySectionHeaders/StickySectionHeaders/MoviesTableViewController.swift
@@ -19,99 +19,78 @@ class MoviesTableViewController: UITableViewController {
     
     var cellId = "cellId"
     
-    var sectionHeaders = [String]()
+    var sortedFirstLetters = [String]()
+    
+    var sections = [[Movie]]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Movies"
         
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
+        // Register tableview cell
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         
-        
+        // Get data from Json file
         let movieManager = MoviesManager()
         moviesDataSource = movieManager.getMovieDataFromJson()
         
         
-        // Set up the ABC section headings
-        let alphabetLetters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-        sectionHeaders = alphabetLetters.components(separatedBy: " ")
+        // Map all first letters into separate array
+        let firstLetters = moviesDataSource.map { $0.titleFirstLetter }
+        
+        // Remove duplicates
+        let uniqueFirstLetters = Array(Set(firstLetters))
+        
+        // Sort them, this is the index
+        self.sortedFirstLetters = uniqueFirstLetters.sorted()
+        self.sections = sortedFirstLetters.map { firstLetter in
+            return moviesDataSource
+                .filter { $0.titleFirstLetter == firstLetter } // get the titles with matching first letter
+                .sorted { $0.title < $1.title } // sort them and saved into new array (sections)
+        }
         
     }
     
     // MARK: - Table view data source
 
+    // SECTION METHODS
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return sectionHeaders.count
+        return sections.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
-        //return moviesDataSource.count
-    }
-
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
-        return sectionHeaders[section]
+        return sortedFirstLetters[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        // this is the method for the floating index on right side of tableview
+        return sortedFirstLetters
+    }
+    
+    
+    // ROW METHODS
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        return sections[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
 
-        cell.textLabel?.text = moviesDataSource[indexPath.row].title
+        cell.textLabel?.text = sections[indexPath.section][indexPath.row].title
 
         return cell
     }
     
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+extension Movie {
+    var titleFirstLetter: String {
+        return String(self.title[self.title.startIndex]).uppercased()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
